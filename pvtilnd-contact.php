c18341532ebc302291f6bc9c2cea699b21a0279f
rogermawer <rogermawer@gmail.com>
2018-08-27 18:41:01 -0700

    change

--- pvtilnd-contact.php
+++ pvtilnd-contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'PVTILND <no-reply@pvtilnd.com>';
+$from = 'PVTILND <roger@mangomeat.com>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'PVTILND <rogermawer@gmail.com>';
