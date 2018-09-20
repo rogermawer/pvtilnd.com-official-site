e171cf4c1feacb4a799659ede922ccfc9b3fc443
rogermawer <rogermawer@gmail.com>
2018-09-19 18:30:18 -0700

    cleanup

--- js/custom.js
+++ js/custom.js
@@ -61,5 +61,47 @@ function shuffle(array) {
     return array;
 }
 
+        $(document).ready(function(){
+            var clicked = true;
+            $('#links').click(function(){
+                if (clicked === true){
+                    $('#socials').stop().fadeIn(800);
+                    clicked = false;
+
+                }else{
+                    $('#socials').stop().fadeOut(800);
+                    clicked = true;
+                }
+            });
+        });
+      
+        
+   $(document).ready(function(){
+        $('.nav-item').click(function(){
+            $(this).next().fadeIn(800);
+            // save hash in url in case back button is pushed
+            var elementID = $(this).attr('id');
+            elementID = '#' + elementID;
+            // if back button is pushed, close box
+            window.onpopstate = function(event) {
+                if (!(window.location.hash === elementID)){
+                     $('.content-box').fadeOut(100);
+                }
+            }
+        });
+       // else, close box with 'x'
+        $('.boxclose').click(function(){
+            $('.content-box').fadeOut(100);
+            // then remove hash from url
+            removeHash();
+        });
+   });
+        
+function removeHash () { 
+    // treat box close like a back button
+    window.history.back();
+}
+        
+
 
 
